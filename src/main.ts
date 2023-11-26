5d0b2868442ac3c94108111fe62ccc809787d59c
Matei <mateisr@proton.me>
2023-11-26 03:04:44 +0200

    Update website

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/styles/style.css'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-createApp(App).mount('#app')
+// Add Font Awesome icon packs
+library.add(fas, far, fab);
+
+// Register the FontAwesome component globally
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
