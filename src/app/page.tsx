d3b2ebe065827f6253d676957dfab4e8ab1e19b8
MateiSR <MateiSR@proton.me>
2025-04-24 04:17:47 +0300

    feat: responsive using flex-1 instead of w/h-{}; hover workaround

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-      <div className="flex flex-row h-full w-full gap-4">
-        <div className="tile flex flex-col items-center justify-center w-1/2 p-4">
+      <div className="flex flex-col md:flex-row h-full w-full gap-4">
+        <div className="tile flex flex-col items-center justify-center flex-1 p-4">
           <p>Tile 1</p>
         </div>
-        <div className="tile flex flex-col items-center justify-center w-1/2 p-4">
+        <div className="tile flex flex-col items-center justify-center flex-1 p-4">
           <p>Tile 2</p>
         </div>
       </div>
