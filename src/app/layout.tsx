f19ce967ec62c49b01199a1bdfe866ef13208f75
MateiSR <MateiSR@proton.me>
2025-05-19 20:22:40 +0300

    fix: notify dark reader of dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="darkreader-lock" />
+      </head>
       <body className={`${firaCode.className} antialiased h-screen w-screen flex flex-col`}>
         <div className="m-4 mb-0">
           <Navbar />
